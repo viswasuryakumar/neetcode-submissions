b78710ea8b75600dac08d401afbe361a81e0c90b
viswasuryakumar <162449152+viswasuryakumar@users.noreply.github.com>
2026-07-01 15:14:05 -0700

    Add: insert-new-interval - submission-1

---
+++ Data Structures & Algorithms/insert-new-interval/submission-1.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int[][] insert(int[][] intervals, int[] newInterval) {
+        List<int[]> res = new ArrayList<>();
+        int n = intervals.length, i =0;
+        while(i<n && intervals[i][1] < newInterval[0]){
+            res.add(intervals[i++]);
+        }
+        while(i<n && intervals[i][1]>=newInterval[0] && intervals[i][0] <= newInterval[1]){
+            newInterval[0] = Math.min(intervals[i][0], newInterval[0]);
+            newInterval[1] = Math.max(intervals[i][1],newInterval[1]);
+            i++;
+        }
+        res.add(newInterval);
+        while(i<n) res.add(intervals[i++]);
+        return res.toArray(new int[res.size()][]);
+    }
+}
