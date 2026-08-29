13186a5d247b12b9bb0be431bbd8f947a8ae5b1c
viswasuryakumar <162449152+viswasuryakumar@users.noreply.github.com>
2026-08-29 16:12:06 -0700

    Add: multiply-strings - submission-1

---
+++ Data Structures & Algorithms/multiply-strings/submission-1.java
@@ -0,0 +1,33 @@
+class Solution {
+    public String multiply(String num1, String num2) {
+        if (num1.equals("0") || num2.equals("0")) return "0";
+        
+        int m = num1.length(), n = num2.length();
+        int[] result = new int[m + n];
+        
+        for (int i = m - 1; i >= 0; i--) {
+            int d1 = num1.charAt(i) - '0';
+            for (int j = n - 1; j >= 0; j--) {
+                int d2 = num2.charAt(j) - '0';
+                int product = d1 * d2;
+                
+                int p1 = i + j;       // position for carry
+                int p2 = i + j + 1;   // position for current digit
+                
+                int sum = product + result[p2];
+                
+                result[p2] = sum % 10;
+                result[p1] += sum / 10;
+            }
+        }
+        
+        StringBuilder sb = new StringBuilder();
+        for (int num : result) {
+            if (!(sb.length() == 0 && num == 0)) {
+                sb.append(num);
+            }
+        }
+        
+        return sb.length() == 0 ? "0" : sb.toString();
+    }
+}
